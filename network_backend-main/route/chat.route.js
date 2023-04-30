1cbfc9007c43732e5fd95b76cb29aa752c4cb05e
Pobrawee Tahtiyom <pobraweesk@gmail.com>
2023-04-30 22:48:24 +0700

    add chat.route

--- network_backend-main/route/chat.route.js
+++ network_backend-main/route/chat.route.js
@@ -1,7 +1,12 @@
 import express from "express";
-import {createChat, toggleNotiChat} from "../controllers/chat.controller.js";
+import {createChat, getChatRoomUserHave, getChatRoomDM, showGroupMember, getAllChatRoomInServer, haveChat, joinChat} from "../controllers/chat.controller.js";
 const router = express.Router();
 
-router.route("/chat").post(createChat);
-router.route("/chat/:chatId").post(createChat);
+router.route("/chat/create").post(createChat);
+//router.route("/chat/have").get(getChatRoomUserHave);
+router.route("/chat/DM").get(getChatRoomDM); //หรือpost (may create new chat)
+router.route("/chat/member").get(showGroupMember);
+router.route("/chat/all").get(getAllChatRoomInServer);
+router.route("/chat/have").get(haveChat);
+router.route("/chat/join").post(joinChat);
 export default router;
