6062b136ea9b0e48ff1a67b84b5bc6cf21f1021e
Pobrawee Tahtiyom <pobraweesk@gmail.com>
2023-04-30 17:45:14 +0700

    requirement 4,5,7

--- network_backend-main/controller/chat.controller.js
+++ network_backend-main/controller/chat.controller.js
@@ -5,8 +5,8 @@ import User from "../models/user.model.js";
 
 export const createChat = async (req, res, next) => {
   try {
-    const {allowedUsers, matchID} = req.body;
-    const chat = new Chat({name: "", allowedUsers, matchID});
+    const {allowedUsers,nameChat} = req.body;
+    const chat = new Chat({nameChat, allowedUsers});
     await chat.save();
     // Add the new room to all allowed users' chatRooms
     await User.updateMany(
@@ -18,26 +18,32 @@ export const createChat = async (req, res, next) => {
     res.status(500).json({error: error.message});
   }
 };
-export const toggleNotiChat = async (req, res, next) => {
-  const chat_id = req.headers.chat_id;
-  const user_id = req.headers.user_id;
-  let user;
+
+export const getAllChatInServer = async (req, res, next) => {
   try {
-    user = await User.findById(user_id);
-    if (user == null) {
-      return res.status(404).send({message: "Cannot find user"});
-    }
-  } catch (err) {
-    return res.status(500).json({message: err.message});
+    let chats = await Chat.find();
+    res.send(chats);
+  } catch (error) {
+    res.status(500).json({message: error.message});
   }
-  const index = user.muteList.indexOf(chat_id);
-  if (index !== -1) {
-    user.muteList.splice(index, 1);
-    res.send("Turn on notification for this chat");
+};
+
+export const joinChat = async (req, res, next) => {
+  const user_id = req.header.user_id;
+  const chat_id = req.header.chat_id;
+  try {
+    await Chat.findOneAndUpdate(
+      {_id: chat_id},
+      {$addToSet: {allowedUsers: user_id}},
+      {new: true}
+    );
+    await User.findOneAndUpdate(
+      {_id: user_id},
+      {$addToSet: {chatRooms: chat_id}},
+      {new: true}
+    );
+    res.send("Join chat successfully");
+  } catch (error) {
+    res.status(500).json({message: error.message});
   }
-  else{
-    user.muteList.push(chat_id);
-    res.send("Turn off notification for this chat");
-  }
-  user.save();
 };
