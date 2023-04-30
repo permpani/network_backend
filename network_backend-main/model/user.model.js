80d4a14fc743007c488318d7e2f66ef200ada32a
Permrawee <permrawee@gmail.com>
2023-04-30 16:09:00 +0700

    fix model

--- network_backend-main/model/user.model.js
+++ network_backend-main/model/user.model.js
@@ -11,11 +11,6 @@ const secret = process.env.JWT_SECRET;
 
 const UserSchema = new mongoose.Schema(
   {
-    googleId: {
-      type: String,
-      sparse: true,
-      unique: true,
-    },
     username: {
       type: String,
       lowercase: true,
@@ -27,130 +22,16 @@ const UserSchema = new mongoose.Schema(
       ],
       index: true,
     },
-    email: {
-      type: String,
-      lowercase: true,
-      unique: true,
-      required: [true, "can't be blank"],
-      match: [/\S+@\S+\.\S+/, "is invalid"],
-      index: true,
-    },
-    image: {
-      type: String,
-      default: "default.png",
-    },
-    status: {
-      type: String,
-      required: [true, "can't be blank"],
-      enum: ["Unverified", "Rejected", "Verified"],
-      default: "Unverified",
-    },
-    owncars: {
-      type: [String],
-      default: [],
-    },
-    role: {
-      type: String,
-      required: [true, "can't be blank"],
-      enum: ["renter", "lessor", "admin"],
-      default: "renter",
-    },
-    firstName: {
-      type: String,
-      default: "",
-    },
-    lastName: {
-      type: String,
-      default: "",
-    },
-    phoneNumber: {
-      type: String,
-      default: "",
-    },
-    rating: {
-      type: Number,
-      required: [true, "can't be blank"],
-      default: 5,
-      min: 0,
-      max: 5,
-    },
-    prefix: {
-      type: String,
-      enum: ["(Not Specific)", "Mr.", "Mrs.", "Miss", "Ms.", "not set"],
-      default: "not set",
-    },
-    rentedCount: {
-      type: Number,
-      default: 0,
-    },
-    rentedOutCount: {
-      type: Number,
-      default: 0,
-    },
-    drivingLicenseNumber: {
-      type: String,
-      default: "",
-    },
-    IDCardNumber: {
-      type: String,
-      default: "",
-    },
-    drivingLicenseImage: {
-      type: String,
-      default: "",
-    },
-    IDCardImage: {
-      type: String,
-      default: "",
-    },
-    omiseCustomerId: {
-      type: String,
-      default: null,
-    },
-    omiseRecipientId: {
-      type: String,
-      default: null,
-    },
     chatRooms: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Chat",
       },
     ],
-    muteList: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Chat",
-      },
-    ],
-    requestTobeLessor: {
-      type: Boolean,
-      default: false,
-    },
-    requestToverifyDate: {
-      type: Date,
-      default: Date.now,
-    },
-    hash: String,
-    salt: String,
   },
   {timestamps: true}
 );
 UserSchema.plugin(findOrCreate);
-
-UserSchema.methods.validPassword = function (password) {
-  var hash = crypto
-    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
-    .toString("hex");
-  return this.hash === hash;
-};
-
-UserSchema.methods.setPassword = function (password) {
-  this.salt = crypto.randomBytes(16).toString("hex");
-  this.hash = crypto
-    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
-    .toString("hex");
-};
 
 UserSchema.methods.generateJWT = function () {
   var today = new Date();
@@ -178,66 +59,6 @@ UserSchema.methods.getIdJSON = function () {
   };
 };
 
-UserSchema.methods.getUserInfoJSON = async function () {
-  let imageUrl;
-  if (this.image.startsWith("https://lh3.googleusercontent.com")) {
-    imageUrl = this.image;
-  } else {
-    imageUrl = await getImageUrl(
-      process.env.GCS_PROFILE_BUCKET,
-      null,
-      this.image
-    );
-  }
-  return {
-    username: this.username,
-    email: this.email,
-    firstName: this.firstName,
-    lastName: this.lastName,
-    phoneNumber: this.phoneNumber,
-    prefix: this.prefix,
-    ownProducts: this.ownProducts,
-    image: imageUrl,
-    IDCardImage: this.IDCardImage,
-    IDCardNumber: this.IDCardNumber,
-    drivingLicenseImage: this.drivingLicenseImage,
-    drivingLicenseNumber: this.drivingLicenseNumber,
-    prefix: this.prefix,
-    role: this.role,
-    rating: this.rating,
-    rentedCount: this.rentedCount,
-    rentedOutCount: this.rentedOutCount,
-    status: this.status,
-    requestTobeLessor: this.requestTobeLessor,
-  };
-};
-
-UserSchema.methods.getNavbarInfoJSON = async function () {
-  let imageUrl;
-  if (this.image.startsWith("https://lh3.googleusercontent.com")) {
-    imageUrl = this.image;
-  } else {
-    imageUrl = await getImageUrl(
-      process.env.GCS_PROFILE_BUCKET,
-      null,
-      this.image
-    );
-  }
-
-  return {
-    username: this.username,
-    status: this.status,
-    user_id: this._id,
-    image: imageUrl,
-    role: this.role,
-    requestTobeLessor: this.requestTobeLessor,
-    haveVerificationInfo: !!(
-      this.drivingLicenseImage &&
-      this.IDCardImage &&
-      true
-    ),
-  };
-};
 
 const User = mongoose.model("User", UserSchema);
 export default User;
